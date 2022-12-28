542204e69dcabeefb4132b9b11661f88d77bce04
Angelo <racering11@gmail.com>
2022-12-28 15:30:29 -0300

    Backup for a while :3

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,7 +2,7 @@ import {Router, Request, Response } from "express";
 
 const createUser = (req: Request, res: Response) => {
     // do something
-    
+    res.status(200).send(req.body);
 }
 
 export default createUser;
